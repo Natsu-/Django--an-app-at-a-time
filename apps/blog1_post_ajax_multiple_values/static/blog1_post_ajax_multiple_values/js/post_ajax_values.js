417597f4b3cc635c2c93305556d6c53d191abec6
Natsu- <romain.garrigues.cs@gmail.com>
2014-03-17 00:46:06 +0100

    Adding other solutions, json-like, or with ajax traditional settings to true

--- apps/blog1_post_ajax_multiple_values/static/blog1_post_ajax_multiple_values/js/post_ajax_values.js
+++ apps/blog1_post_ajax_multiple_values/static/blog1_post_ajax_multiple_values/js/post_ajax_values.js
@@ -9,17 +9,45 @@ function postWithValues(urlToPost, selector, values) {
     });
 }
 
-
 $(document).ready(function() {
 
     $('form').on('submit', function(event) {
         event.preventDefault();
         
         var urlToPost = $(this).attr('action');
-        // Doing serialize on form values
         
+        // Doing jQuery serialize() on form values
         var values = $(this).serialize();
         postWithValues(urlToPost, '#serialize-jquery', values);
+
+        // Getting values separately, and adding each one to an array
+        // We here want to "emulation" a full JS Array initialization
+        // and put it in POST params sent to server
+        var valuesArray = [];
+        $(this).find(':selected').each(function() {
+            valuesArray.push($(this).val());
+        });
+        values = {'params': valuesArray}
+        postWithValues(urlToPost, '#raw-array', values);
+
+        // Same, but respecting "serialize()/param()" logic
+        var valuesArray2 = [];
+        $(this).find(':selected').each(function() {
+            valuesArray2.push({'name': 'params', 'value': $(this).val()});
+        });
+        postWithValues(urlToPost, '#serialize-jquery-manual', valuesArray2);
+
+        // Same, but with json logic
+        var valuesArray3 = [];
+        $(this).find(':selected').each(function() {
+            valuesArray3.push($(this).val());
+        });
+        json_data = JSON.stringify(valuesArray3);
+        postWithValues(urlToPost, '#json-data', {'params': json_data});
+
+        // Same, but with ajaxSettings traditional to "true"
+        $.ajaxSettings.traditional = true;
+        postWithValues(urlToPost, '#ajax-traditional', values);
 
     });
 });
--- apps/blog1_post_ajax_multiple_values/views.py
+++ apps/blog1_post_ajax_multiple_values/views.py
@@ -4,8 +4,10 @@ import json
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def index(request):
 
     if request.is_ajax() and request.method == 'POST':
